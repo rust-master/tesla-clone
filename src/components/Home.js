67cfb8700b3c47eeda417907a79aa5c61af162b2
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2022-02-10 18:12:35 +0500

    ADD: Sections for the TELSA

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,59 @@ import Section from "./Section";
 function Home() {
   return (
     <Container>
-      <Section />
+      <Section
+        title="Model S"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-s.jpg"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Model Y"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-y.jpg"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+      />
+
+      <Section
+        title="Model 3"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-3.jpg"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+      />
+
+      <Section
+        title="Model x"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-x.jpg"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+      />
+
+      <Section
+        title="Solar Panels"
+        description="Lowest Cost Solar Panels in America"
+        backgroundImg="solar-panel.jpg"
+        leftBtnText="Order Now"
+        rightBtnText="Learn More"
+      />
+
+      <Section
+        title="Solar Roof"
+        description="Produce Clean Energy From Your Roof"
+        backgroundImg="solar-roof.jpg"
+        leftBtnText="Order Now"
+        rightBtnText="Learn More"
+      />
+
+      <Section
+        title="Accessories"
+        description=""
+        backgroundImg="accessories.jpg"
+        leftBtnText="Shop Now"
+      />
     </Container>
   );
 }
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,17 +1,23 @@
 import React from "react";
 import styled from "styled-components";
 
-function Section() {
+function Section({
+  title,
+  description,
+  leftBtnText,
+  rightBtnText,
+  backgroundImg,
+}) {
   return (
-    <Wrap>
+    <Wrap bgImage={backgroundImg}>
       <ItemText>
-        <h1>Model S</h1>
-        <p>Order Online for Touchless Delivery</p>
+        <h1>{title}</h1>
+        <p>{description}</p>
       </ItemText>
       <Buttons>
         <ButtonGroup>
-          <LeftButton>Custom Order</LeftButton>
-          <RightButton>Existing Inventory</RightButton>
+          <LeftButton>{leftBtnText}</LeftButton>
+          {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
         </ButtonGroup>
         <DownArrow src="/images/down-arrow.svg" />
       </Buttons>
@@ -32,6 +38,7 @@ const Wrap = styled.div`
   flex-direction: column;
   justify-content: space-between; // vertical
   align-items: center; // horizontal
+  background-image: ${(props) => `url("/images/${props.bgImage}")`};
 `;
 
 const ItemText = styled.div`
@@ -42,6 +49,9 @@ const ItemText = styled.div`
 const ButtonGroup = styled.div`
   display: flex;
   margin-bottom: 30px;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
 `;
 
 const LeftButton = styled.div`
@@ -61,7 +71,11 @@ const LeftButton = styled.div`
 `;
 
 // same as LeftButton, inherit the left button style
-const RightButton = styled(LeftButton)``;
+const RightButton = styled(LeftButton)`
+  background: #fff;
+  opacity: 0.56;
+  color: #000;
+`;
 
 const DownArrow = styled.img`
   height: 40px;
